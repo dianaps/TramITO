d3045a48615e6f6e2c03b652c9e888abb61534ab
Diana Alejandra <75750200+dianaps@users.noreply.github.com>
2022-03-08 13:52:22 -0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre TramITO</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+	<link rel="icon" href="img/logo.png">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <?php include "sections/header.php" ?>
+    <!-- Jumbotron -->
+	<div class="p-5 text-center bg-light">
+		<h1 class="mb-3">Sobre TramITO</h1>
+		<h4 class="mb-3">Algo aquí</h4>
+        <p>TramITO busca proporcionar información</p>
+	</div>
+	<!-- Jumbotron -->
+</body>
+</html>
