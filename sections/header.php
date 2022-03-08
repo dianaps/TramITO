869b6e6f5314adc8dd4663496c41613709dbb546
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-03-08 14:06:51 -0600

    Add folder Sections

---
+++ sections/header.php
@@ -0,0 +1,39 @@
+<header>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container-fluid">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#menu"
+        aria-controls="menu"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="menu">
+      <a class="navbar-brand" href="home.php">TramITO</a>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+          <li class="nav-item active">
+            <a class="nav-link" aria-current="page" href="home.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sobre TramITO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li>
+          <!-- <li class="nav-item">
+            <img src="uploads/<?=$user['p_p']?>"
+    			         class="w-25 rounded-circle" alt="">
+             <a class="nav-link" href="#">Contacto</a> 
+          </li> -->
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Navbar -->
+
+</header>
