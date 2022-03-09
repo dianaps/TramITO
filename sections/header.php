f62fb2524fe3415e66e7fcf471b10befbc3966d3
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-03-09 09:02:34 -0600

    Modifiqué rutas de nav

--- sections/header.php
+++ sections/header.php
@@ -1,6 +1,6 @@
 <header>
   <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
       <button
         class="navbar-toggler"
@@ -9,21 +9,28 @@
         data-bs-target="#menu"
         aria-controls="menu"
         aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+        aria-label="Toggle navigation">
+      
+       
       <span class="navbar-toggler-icon"></span>
+        
       </button>
       <div class="collapse navbar-collapse" id="menu">
-      <a class="navbar-brand" href="home.php">TramITO</a>
         <ul class="navbar-nav me-auto mb-2 mb-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-          <li class="nav-item active">
-            <a class="nav-link" aria-current="page" href="home.php">Home</a>
+        <li class="logo-image">
+            <img rel="icon" src="img/logo.png" class="img-fluid">
+          </li>  
+        <li class="nav-item active">
+            <a class="nav-link" aria-current="page" class="navbar navbar-dark bg-primary" href="home.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Sobre TramITO</a>
+            <a class="nav-link" class="navbar navbar-dark bg-primary" href="#">Chatbot</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Contacto</a>
+            <a class="nav-link" class="navbar navbar-dark bg-primary" href="departamentos.php">Departamentos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" class="navbar navbar-dark bg-primary" href="#">Contacto</a>
           </li>
           <!-- <li class="nav-item">
             <img src="uploads/<?=$user['p_p']?>"
@@ -31,6 +38,8 @@
              <a class="nav-link" href="#">Contacto</a> 
           </li> -->
         </ul>
+        <a href="logout.php"
+    			   class="btn btn-light">Cerrar sesión</a>
       </div>
     </div>
   </nav>
