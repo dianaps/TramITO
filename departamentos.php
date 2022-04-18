d48fa09b3df43ae0642098392fa5418b396a1e5e
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-04-17 22:02:51 -0500

    Change path to image

--- departamentos.php
+++ departamentos.php
@@ -25,7 +25,7 @@ if (isset($_SESSION['username'])) {
 		/>
 		<link rel="stylesheet" href="css/style.css" />
 		<link rel="stylesheet" href="css/cards.css" />
-		<link rel="icon" href="img/logo.png" />
+		<link rel="icon" href="img/logo-buho.png" />
 		<link
 			rel="stylesheet"
 			href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
