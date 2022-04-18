2d81ee2faaaf83a3b0d857a72c37cbc458833127
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-04-18 13:45:24 -0500

    Fix user not found error

--- app/http/auth.php
+++ app/http/auth.php
@@ -1,77 +1,83 @@
-<?php  
+<?php
 session_start();
 
 # check if username & password  submitted
-if(isset($_POST['username']) &&
-   isset($_POST['password'])){
+if (isset($_POST['username']) &&
+ isset($_POST['password'])) {
 
-   # database connection file
-   include '../db.conn.php';
-   
-   # get data from POST request and store them in var
-   $password = $_POST['password'];
-   $username = $_POST['username'];
-   
-   #simple form Validation
-   if(empty($username)){
-      # error message
-      $em = "Usuario es requerido";
+ # database connection file
+ include '../db.conn.php';
 
-      # redirect to 'index.php' and passing error message
-      header("Location: ../../index.php?error=$em");
-   }else if(empty($password)){
-      # error message
-      $em = "Contraseña es requerida";
+ # get data from POST request and store them in var
+ $password = $_POST['password'];
+ $username = $_POST['username'];
 
-      # redirect to 'index.php' and passing error message
-      header("Location: ../../index.php?error=$em");
-   }else {
-      $sql  = "SELECT * FROM 
+ #simple form Validation
+ if (empty($username)) {
+  # error message
+  $em = "Usuario es requerido";
+
+  # redirect to 'index.php' and passing error message
+  header("Location: ../../index.php?error=$em");
+ } else if (empty($password)) {
+  # error message
+  $em = "Contraseña es requerida";
+
+  # redirect to 'index.php' and passing error message
+  header("Location: ../../index.php?error=$em");
+ } else {
+  $sql = "SELECT * FROM
                users WHERE username=?";
-      $stmt = $conn->prepare($sql);
-      $stmt->execute([$username]);
+  $stmt = $conn->prepare($sql);
+  $stmt->execute([$username]);
 
-      # if the username is exist
-      if($stmt->rowCount() === 1){
-        # fetching user data
-        $user = $stmt->fetch();
+  # if the username is exist
+  if ($stmt->rowCount() === 1) {
+   # fetching user data
+   $user = $stmt->fetch();
 
-        # if both username's are strictly equal
-        if ($user['username'] === $username) {
-           
-           # verifying the encrypted password
-          if (password_verify($password, $user['password'])) {
+   # if both username's are strictly equal
+   if ($user['username'] === $username) {
 
-            # successfully logged in
-            # creating the SESSION
-            $_SESSION['username'] = $user['username'];
-            $_SESSION['name'] = $user['name'];
-            $_SESSION['user_id'] = $user['user_id'];
+    # verifying the encrypted password
+    if (password_verify($password, $user['password'])) {
 
-            # redirect to 'home.php' or 'home-admin'
-            if (is_numeric($_SESSION['username'])){
-              header("Location: ../../home.php");
-            }else{
-              header("Location: ../../home-admin.php");
-            }
+     # successfully logged in
+     # creating the SESSION
+     $_SESSION['username'] = $user['username'];
+     $_SESSION['name']     = $user['name'];
+     $_SESSION['user_id']  = $user['user_id'];
 
-          }else {
-            # error message
-            $em = "El usuario o contraseña son incorrectos";
+     # redirect to 'home.php' or 'home-admin'
+     if (is_numeric($_SESSION['username'])) {
+      header("Location: ../../home.php");
+     } else {
+      header("Location: ../../home-admin.php");
+     }
 
-            # redirect to 'index.php' and passing error message
-            header("Location: ../../index.php?error=$em");
-          }
-        }else {
-          # error message
-          $em = "El usuario o contraseña son incorrectos";
+    } else {
+     # error message
+     $em = "El usuario o contraseña son incorrectos";
 
-          # redirect to 'index.php' and passing error message
-          header("Location: ../../index.php?error=$em");
-        }
-      }
+     # redirect to 'index.php' and passing error message
+     header("Location: ../../index.php?error=$em");
+    }
+   } else {
+    # error message
+    $em = "El usuario o contraseña son incorrectos";
+
+    # redirect to 'index.php' and passing error message
+    header("Location: ../../index.php?error=$em");
    }
-}else {
-  header("Location: ../../index.php");
-  exit;
+  } else {
+   # error message
+   $em = "El usuario o contraseña son incorrectos";
+
+   # redirect to 'index.php' and passing error message
+   header("Location: ../../index.php?error=$em");
+  }
+ }
+} else {
+ header("Location: ../../index.php");
+ exit;
 }
