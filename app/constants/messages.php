a339dcda8dd6b6ca1438017582306dca0f6f41d4
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-04-26 20:31:05 -0500

    Add messages.php

---
+++ app/constants/messages.php
@@ -0,0 +1,13 @@
+<?php
+class Messages
+{
+ // INFO
+ const STATUS_ONLINE = "En línea";
+ const LAST_SEEN     = "Última vez activo: ";
+
+ const EMPTY_MESSAGES = "No hay mensajes aún, inicie una conversación";
+
+ // ERROR
+ const ERR_USER_NOT_FOUND = "El usuario %s no ha sido encontrado";
+
+}
