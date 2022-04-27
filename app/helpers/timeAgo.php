6b082eb048733ad7b3f63c77dadfe22e327159d0
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-04-26 20:23:15 -0500

    Change Timezone

--- app/helpers/timeAgo.php
+++ app/helpers/timeAgo.php
@@ -2,29 +2,30 @@
 
 // setting up the time Zone
 // It Depends on your location or your P.c settings
-define('TIMEZONE', 'Africa/Addis_Ababa');
+define('TIMEZONE', 'America/Mexico_City');
 date_default_timezone_set(TIMEZONE);
 
-function last_seen($date_time){
+function last_seen($date_time)
+{
 
-   $timestamp = strtotime($date_time);	
-   
-   $strTime = array("second", "minute", "hour", "day", "month", "year");
-   $length = array("60","60","24","30","12","10");
+ $timestamp = strtotime($date_time);
 
-   $currentTime = time();
-   if($currentTime >= $timestamp) {
-		$diff     = time()- $timestamp;
-		for($i = 0; $diff >= $length[$i] && $i < count($length)-1; $i++) {
-		$diff = $diff / $length[$i];
-		}
+ $strTime = array("second", "minute", "hour", "day", "month", "year");
+ $length  = array("60", "60", "24", "30", "12", "10");
 
-		$diff = round($diff);
-		if ($diff < 59 && $strTime[$i] == "second") {
-			return 'Active';
-		}else {
-			return $diff . " " . $strTime[$i] . "(s) ago ";
-		}
-		
-   }
+ $currentTime = time();
+ if ($currentTime >= $timestamp) {
+  $diff = time() - $timestamp;
+  for ($i = 0; $diff >= $length[$i] && $i < count($length) - 1; $i++) {
+   $diff = $diff / $length[$i];
+  }
+
+  $diff = round($diff);
+  if ($diff < 59 && $strTime[$i] == "second") {
+   return 'Active';
+  } else {
+   return $diff . " " . $strTime[$i] . "(s) ago ";
+  }
+
+ }
 }
