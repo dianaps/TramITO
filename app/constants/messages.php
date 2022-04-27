2e4fe0bac69ad02fb794ae7f825432ac751b1ec7
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-04-26 20:49:34 -0500

    Add constants

--- app/constants/messages.php
+++ app/constants/messages.php
@@ -8,10 +8,16 @@ class Messages
  const EMPTY_MESSAGES = "No hay mensajes aún, inicie una conversación";
 
  // ERROR
- const ERR_USER_NOT_FOUND = "El usuario %s no ha sido encontrado";
- const ERR_USERNAME_REQUIRED = "El nombre de usuario es requerido";
- const ERR_PASSWORD_REQUIRED = "La contraseña es requerida";
- const ERR_INCORRECT_USERNAME_OR_PASSWORD = "El usuario o la contraseña son incorrectos"
- const ERR_ = "";
+ const ERR_USER_NOT_FOUND                 = "El usuario %s no ha sido encontrado";
+ const ERR_NAME_REQUIRED                  = "El nombre es requerido";
+ const ERR_USERNAME_REQUIRED              = "El nombre de usuario es requerido";
+ const ERR_PASSWORD_REQUIRED              = "La contraseña es requerida";
+ const ERR_EMAIL_REQUIRED                 = "El correo es requerido";
+ const ERR_INCORRECT_USERNAME_OR_PASSWORD = "El usuario o la contraseña son incorrectos";
+ const ERR_INCORRECT_FILE_EXTENSION       = "Solo se admiten archivos JPG, JPEG y PNG";
+ const ERR_                               = "";
+
+ // SUCCESS
+ const SCS_CREATION_ACCOUNT = "Cuenta creada exitosamente";
 
 }
