a75077cd56a67a84fbc6448080c0000a7c10443b
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-04-26 20:21:47 -0500

    Add use of constants

--- app/ajax/search.php
+++ app/ajax/search.php
@@ -1,29 +1,33 @@
 <?php
+include '../constants/messages.php';
 
 session_start();
 
 # check if the user is logged in
 if (isset($_SESSION['username'])) {
-    # check if the key is submitted
-    if(isset($_POST['key'])){
-       # database connection file
-	   include '../db.conn.php';
+ # check if the key is submitted
+ if (isset($_POST['key'])) {
+  # database connection file
+  include '../db.conn.php';
 
-	   # creating simple search algorithm :) 
-	   $key = "%{$_POST['key']}%";
-     
-	   $sql = "SELECT * FROM users
+  # creating simple search algorithm :)
+  $key = "%{$_POST['key']}%";
+
+  $sql = "SELECT * FROM users
 	           WHERE username
 	           LIKE ? OR name LIKE ?";
-       $stmt = $conn->prepare($sql);
-       $stmt->execute([$key, $key]);
+  $stmt = $conn->prepare($sql);
+  $stmt->execute([$key, $key]);
 
-       if($stmt->rowCount() > 0){ 
-         $users = $stmt->fetchAll();
+  if ($stmt->rowCount() > 0) {
+   $users = $stmt->fetchAll();
 
-         foreach ($users as $user) {
-         	if ($user['user_id'] == $_SESSION['user_id']) continue;
-       ?>
+   foreach ($users as $user) {
+    if ($user['user_id'] == $_SESSION['user_id']) {
+     continue;
+    }
+
+    ?>
        <li class="list-group-item">
 		<a href="chat.php?user=<?=$user['username']?>"
 		   class="d-flex
@@ -37,21 +41,22 @@ if (isset($_SESSION['username'])) {
 
 			    <h3 class="fs-xs m-2">
 			    	<?=$user['name']?>
-			    </h3>            	
+			    </h3>
 			</div>
 		 </a>
 	   </li>
-       <?php } }else { ?>
-         <div class="alert alert-info 
+       <?php }} else {?>
+         <div class="alert alert-info
     				 text-center">
 		   <i class="fa fa-user-times d-block fs-big"></i>
-           The user "<?=htmlspecialchars($_POST['key'])?>"
-           is  not found.
+         <?php echo sprintf(Messages::ERR_USER_NOT_FOUND, htmlspecialchars($_POST['key'])); ?>
+         <!-- El usuario "<?=htmlspecialchars($_POST['key'])?>"
+           no se encuentra. -->
 		</div>
     <?php }
-    }
+ }
 
-}else {
-	header("Location: ../../index.php");
-	exit;
+} else {
+ header("Location: ../../index.php");
+ exit;
 }
