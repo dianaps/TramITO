ba9caf96e0e4d29d6e757d8d0b0e74f7ef90a785
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-04-26 20:37:09 -0500

    Add constants

--- app/constants/messages.php
+++ app/constants/messages.php
@@ -9,5 +9,9 @@ class Messages
 
  // ERROR
  const ERR_USER_NOT_FOUND = "El usuario %s no ha sido encontrado";
+ const ERR_USERNAME_REQUIRED = "El nombre de usuario es requerido";
+ const ERR_PASSWORD_REQUIRED = "La contraseña es requerida";
+ const ERR_INCORRECT_USERNAME_OR_PASSWORD = "El usuario o la contraseña son incorrectos"
+ const ERR_ = "";
 
 }
