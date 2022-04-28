246d2f8118aabd4aa3ea7622499e048320c16ae8
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-04-27 23:27:31 -0500

    Change database name

--- app/db.conn.php
+++ app/db.conn.php
@@ -9,13 +9,13 @@ $pass = "";
 // $pass = "ErSg2710";
 
 # database name
-$db_name = "tramito";
+$db_name = "tramitov1.2";
 
 #creating database connection
 try {
-                $conn = new PDO("mysql:host=$sName;dbname=$db_name",
-                                $uName, $pass);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ $conn = new PDO("mysql:host=$sName;dbname=$db_name",
+  $uName, $pass);
+ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-                echo "Connection failed : " . $e->getMessage();
+ echo "Connection failed : " . $e->getMessage();
 }
