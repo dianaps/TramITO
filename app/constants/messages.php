139ffd0bd9d6522333ade7975c4abe3b76262f7a
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-04-28 20:40:14 -0500

    Add constants

--- app/constants/messages.php
+++ app/constants/messages.php
@@ -2,9 +2,10 @@
 class Messages
 {
  // INFO
- const STATUS_ONLINE  = "En línea";
- const LAST_SEEN      = "Última vez activo: ";
- const EMPTY_MESSAGES = "No hay mensajes aún, inicie una conversación";
+ const STATUS_ONLINE     = "En línea";
+ const LAST_SEEN         = "Última vez activo: ";
+ const EMPTY_MESSAGES    = "No hay mensajes aún, inicie una conversación";
+ const EMPTY_DEPARTMENTS = "No se encontraron departamentos";
 
  // ERROR
  const ERR_USER_NOT_FOUND                 = "El usuario %s no ha sido encontrado";
