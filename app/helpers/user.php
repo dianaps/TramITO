39479c323597f972d495b251376b6a3d75502b7a
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-04-29 00:03:13 -0500

    Get data from users, departments and students table

--- app/helpers/user.php
+++ app/helpers/user.php
@@ -1,16 +1,23 @@
-<?php  
+<?php
 
-function getUser($username, $conn){
-   $sql = "SELECT * FROM users 
-           WHERE username=?";
-   $stmt = $conn->prepare($sql);
-   $stmt->execute([$username]);
-
-   if ($stmt->rowCount() === 1) {
-   	 $user = $stmt->fetch();
-   	 return $user;
-   }else {
-   	$user = [];
-   	return $user;
-   }
+function getUser($user_id, $role, $conn)
+{
+ if (strcmp($role, 'student') === 0) {
+  $sql = "SELECT * FROM users
+           JOIN students ON users.user_id=students.user_id
+           WHERE users.user_id=?";
+ } else if (strcmp($role, 'department') === 0) {
+  $sql = "SELECT * FROM users
+           JOIN departments ON users.user_id=departments.user_id
+           WHERE users.user_id=?";
+ }
+ $stmt = $conn->prepare($sql);
+ $stmt->execute([$user_id]);
+ if ($stmt->rowCount() === 1) {
+  $user = $stmt->fetch();
+  return $user;
+ } else {
+  $user = [];
+  return $user;
+ }
 }
