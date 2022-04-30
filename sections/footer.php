8064dcac5136e33108ee73faa53a95c3760d6c71
marlenetc <91817709+marlenetc@users.noreply.github.com>
2022-04-30 09:52:34 -0500

    Update footer.php

---
+++ sections/footer.php
@@ -0,0 +1,35 @@
+<style>
+body{
+  font-family: 'Roboto', sans-serif;
+  margin: 0;
+  background-color: #bbf1fa;
+}
+
+.bottom-container{
+	background-color: #66BFBF;
+	padding: 20px;
+    text-align: center;
+  }
+  
+.footer-link{
+	color: #11999E;
+	text-decoration: none;
+	font-weight: bold;
+	margin: 20px 25px 50px 25px;
+  }
+
+.footer-text{
+	color: #EAF6F6;
+    text-align: center;
+  }
+</style>
+<footer class="bottom-container">
+    <p>
+      <a class="footer-link" href="http://www.orizaba.tecnm.mx/">ITO</a>
+      <a class="footer-link" href="https://www.facebook.com/Tram-ITO-116150757742955">Facebook TramITO</a>
+      <a class="footer-link" href="https://www.facebook.com/tecorizaba">Facebook ITO</a>
+      <a class="footer-link" href="https://dianaps.github.io/CV">Correo</a>
+</p>
+      <p class="footer-text">© 2022 TramITO</p>
+
+</footer>
