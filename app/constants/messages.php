8394c0e7a393764ed11f71855c78082f2f741999
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-05-03 02:25:29 -0500

    Add constants

--- app/constants/messages.php
+++ app/constants/messages.php
@@ -24,33 +24,34 @@ class Messages
  const ERR_USERNAME_ALREADY_EXISTS        = "Este usuario ya ha sido registrado, verifique su número de control";
  const ERR_EMAIL_ALREADY_EXISTS           = "Este correo electrónico ya ha sido registrado";
  const ERR_INCORRECT_USERNAME_OR_EMAIL    = "El usuario o el email son incorrectos";
+ const ERR_UPDATE_PROFILE_PICTURE         = 'Ocurrió un error al subir la imagen de perfil';
 
  // UPDATE PASSWORD
- const ERR_CURRENT_PASSWORD_REQUIRED     = "La contraseña actual es requerida";
- const ERR_NEW_PASSWORD_REQUIRED         = "La contraseña nueva es requerida";
- const ERR_CONFIRM_PASSWORD_REQUIRED     = "La confirmación de la contraseña es requerida";
- const ERR_DIFFERENT_PASSWORDS_REQUIRED  = "La contraseña nueva no puede ser idéntica a la actual";
- const ERR_DIFFERENT_PASSWORDS           = "Las contraseñas no coinciden";
- const ERR_CURRENT_PASSWORD              = "La contraseña actual no es correcta";
- const SCS_UPDATE_PASSWORD               = "La contraseña se ha actualizado correctamente";  
+ const ERR_CURRENT_PASSWORD_REQUIRED    = "La contraseña actual es requerida";
+ const ERR_NEW_PASSWORD_REQUIRED        = "La contraseña nueva es requerida";
+ const ERR_CONFIRM_PASSWORD_REQUIRED    = "La confirmación de la contraseña es requerida";
+ const ERR_DIFFERENT_PASSWORDS_REQUIRED = "La contraseña nueva no puede ser idéntica a la actual";
+ const ERR_DIFFERENT_PASSWORDS          = "Las contraseñas no coinciden";
+ const ERR_CURRENT_PASSWORD             = "La contraseña actual no es correcta";
+ const SCS_UPDATE_PASSWORD              = "La contraseña se ha actualizado correctamente";
 
  // SUCCESS
- const SCS_CREATION_ACCOUNT     = "Cuenta creada exitosamente";
- const SCS_CREATION_DEPARTMENT  = "Departamento creado exitosamente";
- const SCS_CREATION_ADMIN       = "El administrador se ha registrado correctamente"; 
+ const SCS_CREATION_ACCOUNT    = "Cuenta creada exitosamente";
+ const SCS_CREATION_DEPARTMENT = "Departamento creado exitosamente";
+ const SCS_CREATION_ADMIN      = "El administrador se ha registrado correctamente";
 
  // ADMIN
- const ERR_USERNAME_REQUIRED                    = "El nombre de usuario es requerido";
- const ERR_USERNAME_ADMIN_ALREADY_EXISTS        = "Este nombre de usuario ya ha sido registrado";
- const ERR_NAME_DEPARTMENT_REQUIRED             = "El nombre del departamento es requerido";
- const ERR_INFO_DEPARTMENT_REQUIRED             = "La información del departamento es requerida";
- const ERR_PHONE_DEPARTMENT_REQUIRED            = "El teléfono es requerido";
- const ERR_PHONE_FORMAT                         = "El teléfono debe estar conformado únicamente por 10 dígitos";
- const ERR_BOSS_DEPARTMENT_REQUIRED             = "El nombre del jefe de departmento es requerido";
- const ERR_USERNAME_DEPARTMENT_ALREADY_EXISTS   = "Este usuario ya ha sido registrado. Elija otro, por favor";
+ const ERR_USERNAME_REQUIRED                  = "El nombre de usuario es requerido";
+ const ERR_USERNAME_ADMIN_ALREADY_EXISTS      = "Este nombre de usuario ya ha sido registrado";
+ const ERR_NAME_DEPARTMENT_REQUIRED           = "El nombre del departamento es requerido";
+ const ERR_INFO_DEPARTMENT_REQUIRED           = "La información del departamento es requerida";
+ const ERR_PHONE_DEPARTMENT_REQUIRED          = "El teléfono es requerido";
+ const ERR_PHONE_FORMAT                       = "El teléfono debe estar conformado únicamente por 10 dígitos";
+ const ERR_BOSS_DEPARTMENT_REQUIRED           = "El nombre del jefe de departmento es requerido";
+ const ERR_USERNAME_DEPARTMENT_ALREADY_EXISTS = "Este usuario ya ha sido registrado. Elija otro, por favor";
 
  // XOOCHBOT QA
- const ERR_QUESTION_REQUIRED    = "La pregunta es requerida";
- const ERR_ANSWER_REQUIRED      = "La respuesta es requerida";
- const SCS_ADD_QA               = "La pregunta-respuesta ha sido agregada correctamente";
+ const ERR_QUESTION_REQUIRED = "La pregunta es requerida";
+ const ERR_ANSWER_REQUIRED   = "La respuesta es requerida";
+ const SCS_ADD_QA            = "La pregunta-respuesta ha sido agregada correctamente";
 }
