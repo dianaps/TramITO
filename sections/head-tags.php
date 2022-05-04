4f37ca65611c349ab252f04d193dfb57ee13a7b2
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-05-04 10:09:03 -0500

    Add head-tags.php file: includes repetitive tags

---
+++ sections/head-tags.php
@@ -0,0 +1,12 @@
+<!-- Etiquetas para el head por defecto (icon, css, )-->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="img/logo-buho.png">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<link rel="stylesheet"
+	href="css/style.css">
+<!-- iconos de fontawesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://kit.fontawesome.com/66b2a98c0c.js" crossorigin="anonymous"></script>
+<!-- <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto&family=Yusei+Magic&display=swap" rel="stylesheet"> -->
