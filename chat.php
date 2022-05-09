1e976799661d9a0972b8c078dce9e3da4c70390b
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-05-09 14:30:39 -0500

    update

--- chat.php
+++ chat.php
@@ -128,8 +128,10 @@ if (!empty($chats)) {
 
     </div>
 </div>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-	<?php include "sections/footer.php"?>
 	<script>
 		var scrollDown = function(){
 			let chatBox = document.getElementById('chatBox');
