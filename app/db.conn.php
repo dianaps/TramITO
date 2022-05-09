5583edc5dae78ec75fd9dc32195479c11dc614db
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-05-09 10:21:32 -0500

    update db.conn.php

--- app/db.conn.php
+++ app/db.conn.php
@@ -5,18 +5,18 @@ $sName = "localhost";
 # user name
 $uName = "root";
 # password
-// $pass = "Contrasena123*";
-$pass = "";
+$pass = "Contrasena123*";
+// $pass = "";
 
 # database name
 $db_name = "tramito";
 
 #creating database connection
 try {
-  $conn = new PDO("mysql:host=$sName;dbname=$db_name",
-    $uName, $pass);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  mysqli_set_charset($conn,"utf8")
+ $conn = new PDO("mysql:host=$sName;dbname=$db_name",
+  $uName, $pass);
+ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ mysqli_set_charset($conn, "utf8");
 } catch (PDOException $e) {
-  echo "Connection failed : " . $e->getMessage();
+ echo "Connection failed : " . $e->getMessage();
 }
